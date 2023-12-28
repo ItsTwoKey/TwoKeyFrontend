00d0a2b1e37655ec542958713769cc011f5370f4
akash <trimbakeakash5@gmail.com>
2023-12-28 20:58:25 +0530

    ai summary

--- src/pages/AI.jsx
+++ src/pages/AI.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import LeftArrowBlack from "../assets/leftArrowBlack.svg";
 import Restart from "../assets/restart.svg";
 import Copy from "../assets/copy.svg";
@@ -6,10 +7,37 @@ import AskAI from "../assets/askAi.svg";
 import Assistant from "../assets/assistant.svg";
 import Info from "../assets/info.svg";
 
-let response = "Lorem ipsum dolor sit amet consectetur. ";
-
 const AI = () => {
   const [message, setMessage] = useState("");
+  const [summarizedText, setSummarizedText] = useState({});
+
+  const fetchData = async () => {
+    const options = {
+      method: "POST",
+      url: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize-text",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "4170d6fec5msh4817aca35c9333ap1cae79jsnc7d7a464dda6",
+        "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
+      },
+      data: {
+        lang: "en",
+        text: "##The problem Let's ask ChatGPT to summarize a fresh piece of news:GPT hallucinates. But it is not apparent.This looks pretty legitimate, isn't it? GPT gives a summary for existing links and detects broken links, which apparently means there is a web scraper underneath, woah! Of course not. GPT is just smart. The biggest issue here is that it is not even obvious for the user that this is a pure fantasy of GPT, so I was pretty sure GPT is able to get the new content from the web until I have started doing real and thorough fact checking of the summary it produced.TLDR: if you feed the URL of a random article into GPT, and ask it to summarize, using \"please summarize <url>\" prompt, it hallucinates by just analyzing words in the URL – it does NOT get the actual content of an article (Ok, this is true for the end of March of 2023, I am pretty sure this might change soon with OpenAI plugins).As you can see on this screenshot above, GPT not only hallucinates, it is also smart enough to gaslight the user when the user tries to check how it behaves on a non-existing URL! GPT leverages context knowledge from the chat to understand that the second URL is looking bad.But how do I know?",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      setSummarizedText(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const handleAskAiClick = () => {
     alert(message);
@@ -17,7 +45,7 @@ const AI = () => {
 
   const handleCopyClick = () => {
     const textArea = document.createElement("textarea");
-    textArea.value = response;
+    textArea.value = summarizedText.summary;
     document.body.appendChild(textArea);
     textArea.select();
     document.execCommand("copy");
@@ -59,7 +87,7 @@ const AI = () => {
                 <p className="text-sm font-semibold"> Summary</p>
               </span>
 
-              <p className="text-sm">{response}</p>
+              <p className="text-sm">{summarizedText.summary}</p>
 
               <span className="my-4 flex justify-between">
                 <button className="">
