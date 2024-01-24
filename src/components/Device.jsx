901cf9ece1748e08a37d4db5ec1be5855b9bdfd3
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2024-01-24 23:48:32 +0530

    all set fro device page only backend api linking was left

--- src/components/Device.jsx
+++ src/components/Device.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import DeviceTube from "./devicePage/DeviceTube";
 import SearchTube from "./devicePage/SearchTube";
 
 const Device = () => {
+  const [deviceName, setdeviceName] = useState(null);
+  const updateDevice = (x) => setdeviceName(x);
+  const sample = [
+    { name: "ravi varma", device: "82kd-wi" },
+    { name: "test user", device: "jh-w1i-56" },
+    { name: "reuben", device: "ima14-wi" },
+    { name: "ali electicwala", device: "u4g-d-wi" },
+    { name: "sudanshu", device: "kp-ad-wi" },
+  ];
+
   return (
     <>
       <div className="py-4 px-8 rounded-md h-screen overflow-y-scroll scrollbar-hide">
         <h2 className="text-xl font-semibold p-2">Device Management</h2>
         <hr className="border border-white border-b-[#D8DEE4]" />
         <SearchTube />
-        <DeviceTube />
+        {sample.map((dev, index) => (
+          <DeviceTube
+            deviceObj={dev}
+            key={index}
+            deviceName={deviceName}
+            select={updateDevice}
+          />
+        ))}
       </div>
     </>
   );
--- src/components/devicePage/DeviceTube.jsx
+++ src/components/devicePage/DeviceTube.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import Desktopicon from "../../assets/Desktopicon.svg";
+import LocationIcon from "../../assets/DefaultMarkerComponent.svg";
 
-export default function DeviceTube() {
+export default function DeviceTube(props) {
   const [device, setDevice] = useState(false);
-  const showDevice = () => {
-    device ? setDevice(false) : setDevice(true);
-    console.log(device);
+  const hanleClick = (e) => {
+    e.stopPropagation();
+    if (device) {
+      setDevice(false);
+      props.select(null);
+    } else {
+      setDevice(true);
+      props.select(props.deviceObj.device);
+    }
   };
   return (
     <div
       className="my-4 rounded-lg py-4 px-8 bg-[#F1F1FF] shadow-sm w-full"
-      onClick={showDevice}
     >
-      <div className="flex justify-between items-center">
-        <h1 className="text-lg font-medium tracking-wider uppercase">
-          DQL-458-K
+      <div className="flex justify-between items-center" onClick={hanleClick}>
+        <h1 className="text-lg font-medium tracking-wider uppercase cursor-pointer">
+          {props.deviceObj.device}
         </h1>
-        <div className="flex gap-5 items-center">
+        <div className="flex gap-5 items-center cursor-pointer">
           <span className="text-sm font-normal">Device Type</span>
           <div className="flex items-center justfy-start gap-2">
             <img src={Desktopicon} alt="device" />
@@ -24,22 +30,29 @@ export default function DeviceTube() {
           </div>
         </div>
       </div>
-      <div
-        className={`flex justify-between items-center ${
-          device ? "" : "hidden"
-        }`}
-      >
-        <h1 className="text-lg font-medium tracking-wider uppercase">
-          DQL-458-K
-        </h1>
-        <div className="flex gap-5 items-center">
-          <span className="text-sm font-normal">Device Type</span>
-          <div className="flex items-center justfy-start gap-2">
-            <img src={Desktopicon} alt="device" />
-            <span className="text-sm font-normal">Desktop</span>
+      {props.deviceName === props.deviceObj.device && (
+        <div
+          className={`flex justify-between items-center pt-2 ${
+            device ? "" : "hidden"
+          }`}
+        >
+          <div className="flex gap-2 justify-center items-center">
+            <img src="" alt="" className="rounded-full w-6 h-6" />
+            <span className="font-medium color-[#1C1C1C] text-sm tracking-wider capitalize">
+              {props.deviceObj.name}
+            </span>
           </div>
+          <div className="flex gap-2 justify-center items-center">
+            <img src={LocationIcon} alt="" />
+            <span className="text-[#1C1C1C] font-normal text-xs">
+              Maharashtra, pune, xyz address 245123 india
+            </span>
+          </div>
+          <button className="rounded-lg px-4 py-2 text-white bg-[#E95744] text-sm font-medium tracking-wider">
+            Delete
+          </button>
         </div>
-      </div>
+      )}
     </div>
   );
 }
