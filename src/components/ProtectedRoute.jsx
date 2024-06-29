f22a2e1d986de04d1ec6d88dfa49761ccd111378
IzaanAnwar <mdizaan67@gmail.com>
2024-06-29 18:48:57 +0530

    fix: login - dash loop

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import Loading from "./Loading";
 import secureLocalStorage from "react-secure-storage";
@@ -16,7 +16,15 @@ export function ProtectedRoute() {
   const location = useLocation();
   const [isLoading, setIsLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
+  const data = secureLocalStorage.getItem("profileData");
+  const userDetails = useMemo(() => {
+    try {
+      return JSON.parse(data);
+    } catch (err) {
+      console.error("Could not parse profile data", { err });
+      return null;
+    }
+  }, [data]);
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
@@ -34,12 +42,21 @@ export function ProtectedRoute() {
     return <Loading />;
   }
 
+  if (
+    userDetails &&
+    userDetails?.is_authenticated &&
+    !userDetails?.is_approved
+  ) {
+    console.log("User not approved, redirecting to waiting lobby");
+    return <Navigate to="/waiting-lobby" />;
+  }
+
+  console.log({ isAuthenticated });
   if (!isAuthenticated) {
     console.log("Redirecting to login page", isAuthenticated, isLoading);
     return (
       <Navigate to={"/login"} replace state={{ path: location.pathname }} />
     );
   }
-
   return <Outlet />;
 }
